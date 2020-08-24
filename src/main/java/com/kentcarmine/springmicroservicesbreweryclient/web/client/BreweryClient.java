94fc52125d8a120cbd731d5597e27e4daebae43b
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-24 16:16:03 -0500

    Fixed file tree issue and added consumer for beer get route

---
+++ src/main/java/com/kentcarmine/springmicroservicesbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,34 @@
+package com.kentcarmine.springmicroservicesbreweryclient.web.client;
+
+import com.kentcarmine.springmicroservicesbreweryclient.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "sfg.brewery", ignoreInvalidFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+
+    private String apihost;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public void setApiHost(String apihost) {
+        this.apihost = apihost;
+    }
+}
