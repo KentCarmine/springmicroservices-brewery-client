cd58b3f3e41ae2e33312fe5153e7d3fb9a08cced
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-27 17:53:24 -0500

    Added POST to save new beer and test for that method

--- src/test/java/com/kentcarmine/springmicroservicesbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/kentcarmine/springmicroservicesbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,15 @@ class BreweryClientTest {
         BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+        System.out.println(uri.toString());
     }
 }
